a29667f5bad6f51456b8ca8505782028a46ed9d6
ultra <13109003+UltrusBot@users.noreply.github.com>
2022-08-12 21:12:28 -0400

    Change some template mod stuff

--- src/main/java/me/ultrusmods/teleportationcircles/mixin/TitleScreenMixin.java
+++ src/main/java/me/ultrusmods/teleportationcircles/mixin/TitleScreenMixin.java
@@ -1,6 +1,6 @@
-package com.example.example_mod.mixin;
+package me.ultrusmods.teleportationcircles.mixin;
 
-import com.example.example_mod.ExampleMod;
+import me.ultrusmods.teleportationcircles.TeleportationCirclesMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		TeleportationCirclesMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
